b7a2c3b64899f07cd431e2537fb63a8e7ad41924
MartinWeigl125 <martinweigljun@gmail.com>
2025-12-04 09:06:03 +0100

    feat(charts-page): points per category

--- src/lib/services/placemark-utils.ts
+++ src/lib/services/placemark-utils.ts
@@ -1,22 +1,49 @@
+import { categories } from "$lib/stores";
 import type { Category, Comment, DataSet, MarkerLayer, MarkerSpec, Poi } from "$lib/types/placemark-types";
+import { get } from "svelte/store";
 
+// chart helper functions
 export function generatePerRating(commentList: Comment[]): DataSet {
-    const totalByRating: DataSet = {
-        labels: [1, 2, 3, 4, 5],
-        datasets: [
-            {
-                values: [0, 0, 0, 0, 0]
-            }
-        ]
-    };
+  const totalByRating: DataSet = {
+    labels: [1, 2, 3, 4, 5],
+    datasets: [
+      {
+        values: [0, 0, 0, 0, 0]
+      }
+    ]
+  };
 
-    commentList.forEach((comment) => {
-        totalByRating.datasets[0].values[comment.rating - 1] += 1;
-    });
+  commentList.forEach((comment) => {
+    totalByRating.datasets[0].values[comment.rating - 1] += 1;
+  });
 
-    return totalByRating;
+  return totalByRating;
 }
 
+export function generatePoisPerCategory(): DataSet {
+  const allCategories = get(categories).filter(cat => cat.name.toLowerCase() !== "private points of interest");
+  const names = allCategories.map(cat => cat.name);
+  const values = new Array<number>(names.length).fill(0);
+  allCategories.forEach((cat, i) => {
+    values[i] = cat.pois.length;
+  });
+
+  // order by values descending
+  const combined = names.map((label, i) => ({ label, value: values[i] }));
+  combined.sort((a, b) => b.value - a.value);
+
+  const poisPerCategory: DataSet = {
+    labels: combined.map(e => e.label),
+    datasets: [
+      { values: combined.map(e => e.value) }
+    ]
+  };
+
+  return poisPerCategory;
+}
+
+
+// map helper functions
 export function generateMarkerSpec(poi: Poi): MarkerSpec {
   return <MarkerSpec>{
     id: poi._id,
--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+    import Chart from "svelte-frappe-charts";
     import { placemarkService } from "$lib/services/placemark-service";
+    import { generatePoisPerCategory } from "$lib/services/placemark-utils";
     import { currentSession } from "$lib/stores";
+    import type { DataSet } from "$lib/types/placemark-types";
     import Card from "$lib/ui/Card.svelte";
     import { onMount } from "svelte";
     import { get } from "svelte/store";
+
+    let poisPerCategory: DataSet;
+    let bestPois: DataSet;
+    let usersWithMostPrivatePois: DataSet;
+    let bestCategories: DataSet;
 
     onMount(async () => {
         const pois = await placemarkService.getPois(get(currentSession));
         // get users with private pois only fetches email + name + count of private pois => no sensitive data
         const users = await placemarkService.getUsersWithPrivatePoiCount(get(currentSession));
         const ratings = await placemarkService.getComments(get(currentSession));
+
+        poisPerCategory = generatePoisPerCategory();
     });
 </script>
 
@@ -17,7 +27,7 @@
   <div class="columns">
     <div class="column is-half">
       <Card title="Most Points per Category">
-        <!-- Balkendiagramm -->
+        <Chart data={poisPerCategory} type="bar" height={200} />
       </Card>
       <Card title="Best Points of Interest">
         <!-- Scatterplot: x: häufig bewertet, y: gut bewertet -->

5b39f97fe1982cfdf84f2434f30964645054e204
MartinWeigl125 <martinweigljun@gmail.com>
2025-12-04 08:05:51 +0100

    feat(charts-page): structure for charts

--- src/lib/ui/Menu.svelte
+++ src/lib/ui/Menu.svelte
@@ -16,6 +16,7 @@
   </div>
   <div id="navbarMenu" class="navbar-menu">
     <div class="navbar-end">
+      <a class="navbar-item" href="/charts"> Statistics</a>
       <a class="navbar-item" href="/logout"> Logout [{$currentSession.firstName} {$currentSession.lastName}]</a>
     </div>
     <div></div>
---
+++ src/routes/charts/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Card from "$lib/ui/Card.svelte";
+
+</script>
+
+<div class="container">
+  <div class="columns">
+    <div class="column is-half">
+      <Card title="Most Points per Category">
+        <!-- Balkendiagramm -->
+      </Card>
+      <Card title="Best Points of Interest">
+        <!-- Scatterplot: x: häufig bewertet, y: gut bewertet -->
+      </Card>
+    </div>
+    <div class="column is-half">
+      <Card title="Users with most private Points of Interest">
+        <!-- Kreisdiagramm -->
+      </Card>
+      <Card title="Best rated Categories">
+        <!-- Balkendiagramm -->
+      </Card>
+    </div>
+  </div>
+</div>

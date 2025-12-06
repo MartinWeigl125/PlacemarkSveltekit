1de3773a7a301dd47b2ef95e6ee2dab425088b24
MartinWeigl125 <martinweigljun@gmail.com>
2025-12-06 19:57:06 +0100

    feat(security): oauth with google

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -27,6 +27,21 @@
 {#if message}
   <Message {message} />
 {/if}
+<div class="buttons">
+  <a class="button is-danger is-fullwidth" href="http://localhost:3000/api/users/auth/google">
+    <span class="icon">
+      <i class="fa-brands fa-google"></i>
+    </span>
+    <span>Login with Google</span>
+  </a>
+
+  <a class="button is-dark is-fullwidth" href="http://localhost:3000/api/users/auth/github">
+    <span class="icon">
+      <i class="fa-brands fa-github"></i>
+    </span>
+    <span>Login with GitHub</span>
+  </a>
+</div>
 <form on:submit|preventDefault={login}>
   <UserCredentials bind:email bind:password />
   <button class="button is-success is-fullwidth">Log in</button>
